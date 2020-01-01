e78bf69e2778fa911f338f9415fe9ba4af352fed
Numbericons <zachjoliver@gmail.com>
2020-01-01 12:16:38 -0800

    Add new approach still too complex

--- sections.js
+++ sections.js
@@ -1,9 +1,23 @@
+//thought: dont check when i == 1 instead check when i2 === k3 so that j = 1
+
 function c(k) {
   let count = 0;
-  for (let i = 1; i <= k*2; i++) {
-    for (let j = 1; j <= k*2; j++) {
-      if (k ** 3 === i * i * j * j) count += 1;
+  const k3 = k ** 3;
+  const max = Math.sqrt(k3);
+  let coords = [];
+  for (let i = 1; i <= max; i++) {
+    let i2 = i ** 2;
+    let iHalf2 = (i2 / 2) ** 2;
+    if (i2 * iHalf2 === k3 || i2 === k3) {
+      count += 1;
+      coords.push([i2, iHalf2]);
     }
+    // for (let j = 1; j <= max; j++) {
+    //   if (k3 === i2 * j * j) {
+    //     count += 1;
+    //     coords.push([i,j]);
+    //   }
+    // }
   }
 
   return count;
